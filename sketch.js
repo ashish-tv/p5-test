5ca11d0ceff8d557620a2116c664e7c033b533e3
ashish-tv <136572612+ashish-tv@users.noreply.github.com>
2023-06-15 17:41:55 +0530

    working?

--- sketch.js
+++ sketch.js
@@ -11,13 +11,15 @@ This example uses p5 preload function to create the classifier
 
 // Global variable to store the classifier
 let classifier;
-
+let path="https://labs.bible.org/api/?passage=John+3:16-17&formatting=plain"
+let count=1;
+let lastLabel="";
 // Label (start by showing listening)
 let label = "listening";
-
+let label1="verse";
 // Teachable Machine model URL:
-let soundModelURL = 'https://teachablemachine.withgoogle.com/models/h3p9R41J/model.json';
-
+let soundModelURL = //'https://teachablemachine.withgoogle.com/models/h3p9R41J/model.json';
+'https://teachablemachine.withgoogle.com/models/qjvex6OwC/model.json';
 
 function preload() {
   // Load the model
@@ -29,15 +31,25 @@ function setup() {
   // Start classifying
   // The sound model will continuously listen to the microphone
   classifier.classify(gotResult);
+  httpGet( path,'text',getText)
 }
-
+function getText(verse){label1=verse}
 function draw() {
   background(0);
   // Draw the label in the canvas
+   // textWrap(CHAR);
+
   fill(255);
   textSize(32);
   textAlign(CENTER, CENTER);
-  text(label, width / 2, height / 2);
+  text(label, width / 2, height / 1.5);
+  
+  
+ //rect(10, 200, 200, 150);
+  
+  textSize(10);
+  textAlign(CENTER, CENTER);
+  text(label1, width / 8, -height /32,200,150);
 }
 
 
@@ -49,5 +61,11 @@ function gotResult(error, results) {
   }
   // The results are in an array ordered by confidence.
   // console.log(results[0]);
-  label = results[0].label;
+   label = results[0].label;
+  //if((lastLabel!=label)&&label!='Background Noise')//{lastLabel=label;count=count+1;print("asdf")}
+  if(label=="Matthew"||label=="Mark"||label=="Luke"||label=="John")
+ {count=1;path="https://labs.bible.org/api/?passage="+label+"+"}//John+3:16-17&formatting=plain"}
+  else{if(label!="Background Noise"){if(count==1){path=path+label+":";count=2;}
+      if(count==2){path=path+label+"&formatting=plain";count=3;httpGet( path,'text',getText)}}}
+  print( count,path);
 }
